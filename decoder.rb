f3b6747a219ba639b3e50817eacc94d59205b43c
Leonardo Pareja Pareja <leonardopareja.fl@hotmail.com>
2022-02-23 16:35:57 -0500

    Add alphabet and loops

--- decoder.rb
+++ decoder.rb
@@ -1,5 +1,18 @@
-message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ..."
+message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+ALPHABET = {
+  ".-"=> "A", "-..."=> "B", "-.-."=> "C", "-.."=> "D", "."=> "E", "..-."=> "F", "--."=> "G", "...."=> "H",
+  ".."=> "I", ".---"=> "J", "-.-"=> "K", ".-.."=> "L", "--"=> "M", "-."=> "N", "---"=> "O", ".--."=> "P",
+  "--.-"=> "Q", ".-."=> "R", "..."=> "S", "-"=> "T", "..-"=> "U", "...-"=> "V", ".--"=> "W", "-..-"=> "X", "-.--"=> "Y", "--.."=> "Z"
+}
 
 def decode(message)
-  message.split('   ', -1).map{|item| item.split(' ', -1)}
+  output_message = ''
+  words_array = message.split('   ', -1).map{|item| item.split(' ', -1)}
+  words_array.each do |word| 
+    word.each do |letter| 
+      output_message += ALPHABET[letter] 
+    end
+    output_message += ' '
+  end
+  puts output_message
 end
